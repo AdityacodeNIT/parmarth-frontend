33651cff9e60d6365c643e6746bd8cb7d6ae3078
AdityacodeNIT <srivastavaditya145@gmail.com>
2025-03-13 22:28:55 +0530

    Refactor AddProduct component to use updated API key environment variable

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -18,7 +18,7 @@ const AddProduct = () => {
   const [selectedAttributes, setSelectedAttributes] = useState([]);
   const [selectedAttribute, setSelectedAttribute] = useState("");
 
-  const genAI = new GoogleGenerativeAI(`${import.meta.env.VITE_GEMINI_API_KEY}`);
+  const genAI = new GoogleGenerativeAI(`${import.meta.env.API_KEY}`);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const navigate = useNavigate();
 
